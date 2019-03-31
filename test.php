c2e88c7f90aba3f7971800bd1a5f66e71fcd9ff4
noermuhammad <lastxtreme@yahoo.com>
2019-03-31 19:53:05 +0700

    lagi

--- test.php
+++ test.php
@@ -1,19 +1,29 @@
 <?php
-    $serverName = "webappdicoding.database.windows.net"; // update me
-    $connectionOptions = array(
-        "Database" => "pos", // update me
-        "Uid" => "mingkymumu", // update me
-        "PWD" => "mumu81858591_" // update me
-    );
-    //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT TOP 20 FROM dbo.products";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['title'] . " " . $row['description'] . PHP_EOL);
-    }
-    sqlsrv_free_stmt($getResults);
+    // $serverName = "webappdicoding.database.windows.net"; // update me
+    // $connectionOptions = array(
+    //     "Database" => "pos", // update me
+    //     "Uid" => "mingkymumu", // update me
+    //     "PWD" => "mumu81858591_" // update me
+    // );
+    // //Establishes the connection
+    // $conn = sqlsrv_connect($serverName, $connectionOptions);
+    // $tsql= "SELECT * FROM dbo.products";
+    // $getResults= sqlsrv_query($conn, $tsql);
+    // echo ("Reading data from table" . PHP_EOL);
+    // if ($getResults == FALSE)
+    //     echo (sqlsrv_errors());
+    // while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    //  echo ($row['title'] . " " . $row['description'] . PHP_EOL);
+    // }
+    // sqlsrv_free_stmt($getResults);
+    
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+// phpinfo(INFO_MODULES);
+
+
 ?>
