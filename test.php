bff8f3937822656379757c528c032cf805f8fb2f
noermuhammad <lastxtreme@yahoo.com>
2019-03-31 20:10:38 +0700

    commit ulang

--- test.php
+++ test.php
@@ -1,23 +1,20 @@
 <?php
-   $serverName = "tcp:webappdicoding.database.windows.net,1433";
-   $userName = 'mingkymumu@webappdicoding.database.windows.net';
-   $userPassword = 'mumu81858591_';
-   $dbName = "pos";
-   $table = "products";
+$myServer = "webappdicoding.database.windows.net";
+$myUser = "mingkymumu";
+$myPass = "mumu81858591_";
+$myDB = "pos";
 
-   $connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
-
-   sqlsrv_configure('WarningsReturnAsErrors', 0);
-   $conn = sqlsrv_connect( $serverName, $connectionInfo);
-   if($conn === false)
-   {
-     FatalError("Failed to connect...");
-   }
-// phpinfo();
-
-// Show just the module information.
-// phpinfo(8) yields identical results.
-// phpinfo(INFO_MODULES);
-
-
+$conn = mssql_connect($myServer,$myUser,$myPass);
+if (!$conn)
+{ 
+  die('Not connected : ' . mssql_get_last_message());
+} else
+{
+    echo "Konek cuy";
+}
+$db_selected = mssql_select_db($myDB, $conn);
+if (!$db_selected) 
+{
+  die ('Can\'t use db : ' . mssql_get_last_message());
+} 
 ?>
