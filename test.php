da8e44fca1d80c34ef6da5301e46a486e56bd238
noermuhammad <lastxtreme@yahoo.com>
2019-03-31 19:36:49 +0700

    lagi

--- test.php
+++ test.php
@@ -7,3 +7,23 @@ $myDB = "pos";
 $dbhandle = mssql_connect($myServer, $myUser, $myPass)
   or die("Couldn't connect to SQL Server on $myServer"); 
 ?>
+
+<?php
+    $serverName = "webappdicoding.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "pos", // update me
+        "Uid" => "mingkymumu", // update me
+        "PWD" => "mumu81858591_" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 FROM products";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['title'] . " " . $row['description'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
