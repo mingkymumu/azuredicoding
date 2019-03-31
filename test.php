7d7340f6f4ee8b14d6f8ca7d9c95f73dc7440870
noermuhammad <lastxtreme@yahoo.com>
2019-03-31 20:34:47 +0700

    lagi cok

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$connectionInfo = array("UID" => "mingkymumu@webappdicoding", "pwd" => "{your_password_here}", "Database" => "pos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "mingkymumu@webappdicoding", "pwd" => "mumu81858591_", "Database" => "pos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:webappdicoding.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 if($conn){
