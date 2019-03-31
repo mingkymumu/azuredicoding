9199092051e03a288b854f9647ee3307cb786ee6
noermuhammad <lastxtreme@yahoo.com>
2019-03-31 19:32:59 +0700

    lago

--- test.php
+++ test.php
@@ -4,17 +4,6 @@ $myUser = "mingkymumu";
 $myPass = "mumu81858591_";
 $myDB = "pos";
 
-$conn = mssql_connect($myServer,$myUser,$myPass);
-if (!$conn)
-{ 
-  die('Not connected : ' . mssql_get_last_message());
-} else
-{
-    echo "Konek cuy";
-}
-$db_selected = mssql_select_db($myDB, $conn);
-if (!$db_selected) 
-{
-  die ('Can\'t use db : ' . mssql_get_last_message());
-} 
+$dbhandle = mssql_connect($myServer, $myUser, $myPass)
+  or die("Couldn't connect to SQL Server on $myServer"); 
 ?>
