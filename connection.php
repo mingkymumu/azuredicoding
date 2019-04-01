145856a45a0ed35f7579e76721f5ae87b0ff7cf7
noermuhammad <lastxtreme@yahoo.com>
2019-04-01 09:27:45 +0700

    coba baru

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$connectionInfo = array("UID" => "mingkymumu@webappdicoding", "pwd" => "mumu81858591_", "Database" => "pos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webappdicoding.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if(!$conn){
+    echo "gagal terkoneksi ke database !!";
+}
+else{
+    echo "koneksi sukses!";
+}
+// phpinfo();
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// Create database connection using config file
+include_once("connection.php");
+
+// Fetch all users data from database
+$result = sqlsrv_query($conn, "SELECT * FROM products ORDER BY id DESC");
+?>
+
+<html>
+<head>    
+    <title>Product List</title>
+</head>
+
+<body>
+<!-- <a href="add.php">Add New User</a><br/><br/> -->
+
+    <table width='80%' border=1>
+
+    <tr>
+        <th>Name</th> <th>description</th> 
+    </tr>
+    <?php  
+    while($product_data = sqlsrv_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$product_data['title']."</td>";
+        echo "<td>".$product_data['description']."</td>";
+          
+    }
+    ?>
+    </table>
+</body>
+</html>
