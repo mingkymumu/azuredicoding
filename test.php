7a4a086e6490f2e01e2b4917bad29619c45ccee5
noermuhammad <lastxtreme@yahoo.com>
2019-04-01 09:09:46 +0700

    lagi setting

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
-//  try {
-//     $conn = new PDO("sqlsrv:server = tcp:webappdicoding.database.windows.net,1433; Database = pos", "mingkymumu", "mumu81858591_");
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }
-// catch (PDOException $e) {
-//     print("Error connecting to SQL Server.");
-//     die(print_r($e));
-// }
-phpinfo();
+ try {
+    $conn = new PDO("sqlsrv:server = tcp:webappdicoding.database.windows.net,1433; Database = pos", "mingkymumu", "mumu81858591_");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+// phpinfo();
 ?>
