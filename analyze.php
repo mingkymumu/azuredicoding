cb6dc8b24128bf36e530bc7fd6ce3def7193a5bb
noermuhammad <lastxtreme@yahoo.com>
2019-04-03 19:15:10 +0700

    test

--- analyze.php
+++ analyze.php
@@ -112,11 +112,11 @@ $containerName = "blobgambar";
                 // echo $blob->getName().": ".$blob->getUrl()."<br />";
                 echo "<img id=img".$no." src=".$blob->getUrl()." height=300 width=500 />";
                 echo "<div id='wrapper".$no."' style='width:500px; display:table;'>
-                      <div id='jsonOutput".$no."' style='width:600px; display:table-cell;'>
+                      <div id='jsonOutput".$no."' style='width:400px; display:table-cell;'>
                     Response:
                     <br>
                     <textarea id='responseTextArea".$no."' class='UIInput'
-                              style='width:580px; height:400px;'></textarea>
+                              style='width:400px; height:300px;'></textarea>
                 </div>
                 </div>";
                 echo "<br>";
