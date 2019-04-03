62201e1452bd36b56726883345f08d22a6138c35
noermuhammad <lastxtreme@yahoo.com>
2019-04-03 12:01:31 +0700

    fix upload

--- upload.php
+++ upload.php
@@ -22,7 +22,7 @@ if(isset($_POST['Submit1']))
     $createContainerOptions->addMetaData("key2", "value2");
 	$containerName = "blockblobs".generateRandomString();
 
-    $filepath ="image/" $_FILES["file"]["name"];
+    $filepath = "image/". $_FILES["file"]["name"];
     if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
     {
         $fileToUpload = $filepath;
