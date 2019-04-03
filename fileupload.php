7de3975c871b8480658cf36b5390dbfa0117cc4c
noermuhammad <lastxtreme@yahoo.com>
2019-04-03 11:51:15 +0700

    coba sample

--- fileupload.php
+++ fileupload.php
@@ -14,7 +14,7 @@ Select image :
 <?php
 if(isset($_POST['Submit1']))
 { 
-$filepath = "images/" . $_FILES["file"]["name"];
+$filepath = "image/" . $_FILES["file"]["name"];
 
 if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
 {
